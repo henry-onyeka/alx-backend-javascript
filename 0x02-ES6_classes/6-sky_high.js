7c50bae16924b5906f6b88baf8a385052f2ef034
MICHAEL HENRY ONYEKA <hmike54@gmail.com>
2023-09-15 01:03:30 +0100

    Create 6-sky_high.js

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+import Building from './5-building.js';
+
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
+
+export default SkyHighBuilding;
